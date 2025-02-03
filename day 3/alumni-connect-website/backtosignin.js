1cee994ed9dd54e007b145fed468cbf845baa4a9
Jashu1727 <jashwanthjashu1727@gmail.com>
2025-02-03 23:26:53 +0530

    Removed unwanted files

--- day 3/alumni-connect-website/backtosignin.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let backtosigninLink = document.getElementById("Backtosignin");
-    backtosigninLink.addEventListener("click", function (event) {
-        event.preventDefault(); 
-        window.location.href = "signin.html";
-    });
-});
